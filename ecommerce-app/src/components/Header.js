7e46a1b3496a7ce64e89b4a1916953482e3fe029
patel vraj <122253235+vrajpatel0007@users.noreply.github.com>
2023-11-08 23:50:55 +0530

    Ecommerce

---
+++ ecommerce-app/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { NavLink ,Link } from 'react-router-dom'
+const Header = () => {
+  return <>
+  <header className="header-top-strip">
+    <div className="container-xxl">
+      <div></div>
+    </div>
+  </header>
+  </>
+}
+
+export default Header
---
+++ ecommerce-app/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <BrowserRouter>
+    <StrictMode>
+    <App />
+  </StrictMode>
+  </BrowserRouter>
+);
+
